58417e228bff37f345139c880469b750f29d2d34
hansent <thomas.hansen@gmail.com>
2014-09-03 19:01:56 -0500

    add karma config and basic testing setup

---
+++ test/browser/index.js
@@ -0,0 +1,16 @@
+describe("saskavi ", function() {
+
+  it('should be able to call a remote function', function() {
+    expect(true).to.be.false;
+  });
+
+  it('should invoke the callback with the return value of the remote function', function() {
+    expect(true).to.be.false;
+  });
+
+  it('should be able to call functions with no parameters', function() {
+    expect(true).to.be.false;
+  });
+
+
+});
